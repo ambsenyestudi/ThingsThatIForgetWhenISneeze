22446376805a087b1d1c1c489c7bc976f30ad248
Adria <Adria@DESKTOP-4H3TDN0>
2016-11-04 09:26:06 +0100

    Setting up json data

--- ThingsThatIForgetWhenISneeze/Program.cs
+++ ThingsThatIForgetWhenISneeze/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ThingsThatIForgetWhenISneeze.Application;
 
 namespace ThingsThatIForgetWhenISneeze
 {
@@ -10,6 +11,15 @@ namespace ThingsThatIForgetWhenISneeze
     {
         static void Main(string[] args)
         {
+            JsonManager jsonManager = new JsonManager();
+            foreach(var v in jsonManager.DeserializedData)
+            {
+                //<condition>?<true>:<false>
+                string sValue = v.Value != null ? v.Value.ToString() : "\"null\"";
+                Console.WriteLine(v.Key + "is " + sValue);
+            }
+            //To prevent de program from ending
+            var k = Console.ReadKey();
         }
     }
 }
