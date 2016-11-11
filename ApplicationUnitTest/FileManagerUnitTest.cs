68d02baa90544705f03314541534216f227278b0
Adria <Adria@DESKTOP-4H3TDN0>
2016-11-11 17:23:10 +0100

    File manager addition

---
+++ ApplicationUnitTest/FileManagerUnitTest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThingsThatIForgetWhenISneeze.Application;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using ThingsThatIForgetWhenISneeze.Application.Entities;
+
+namespace ApplicationUnitTest
+{
+    [TestClass]
+    public class FileManagerUnitTest
+    {
+        [TestMethod]
+        public void Test_GetPathExpected_Method()
+        {
+            string fileName = "testFile";
+            string fileExtension = "json";
+            string assertedPath= string.Format(@"{0}\{1}.{2}", AppDomain.CurrentDomain.BaseDirectory, fileName, fileExtension);
+            string filepath=FileManager.GetPath(fileName,fileExtension);
+            Trace.WriteLine(assertedPath);
+            Trace.WriteLine(filepath);
+            Assert.AreEqual(assertedPath, filepath);
+        }
+        [TestMethod]
+        public void Test_NoExtension_Method()
+        {
+            string fileName = "testFile";
+            string fileExtension = "";
+            string filepath = FileManager.GetPath(fileName, fileExtension);
+            Assert.IsNull(filepath);
+        }
+
+        [TestMethod]
+        public void Test_NoName_Method()
+        {
+            string fileName = "";
+            string fileExtension = "json";
+            string filepath = FileManager.GetPath(fileName, fileExtension);
+            Assert.IsNull(filepath);
+        }
+        [TestMethod]
+        public void Test_ExtensionFormat_Method()
+        {
+            string fileName = "testFile";
+            string fileExtension = ".json";
+            string assertedPath = string.Format(@"{0}\{1}{2}", AppDomain.CurrentDomain.BaseDirectory, fileName, fileExtension);
+            string filepath = FileManager.GetPath(fileName, fileExtension);
+            Trace.WriteLine(assertedPath);
+            Trace.WriteLine(filepath);
+            Assert.AreEqual(assertedPath, filepath);
+        }
+        [TestMethod]
+        public void Test_FolderFormat_Method()
+        {
+            string fileName = "testFile";
+            string fileExtension = "json";
+            string folderName = "Tests";
+            string assertedPath = string.Format(@"{0}\{1}\{2}.{3}", AppDomain.CurrentDomain.BaseDirectory,folderName, fileName, fileExtension);
+            string filepath = FileManager.GetPath(fileName, fileExtension,folderName);
+            string secondPath = FileManager.GetPath(fileName, fileExtension, string.Format("{0}{1}",folderName,@"\"));
+            Trace.WriteLine(assertedPath);
+            Trace.WriteLine(filepath);
+            Assert.AreEqual(assertedPath, filepath);
+            Assert.AreEqual(filepath, secondPath);
+        }
+        [TestMethod]
+        public void Test_ReadFileNotNull_Method()
+        {
+            string fileName = "testFile";
+            string fileExtension = "json";
+            string text = FileManager.ReadFile(fileName, fileExtension);
+            Trace.WriteLine(string.Format("text is: {0}",text));
+            Assert.IsNotNull(text);
+        }
+        [TestMethod]
+        public void Test_WriteReadFile_Expected_Method()
+        {
+            string fileName = "testFile";
+            string fileExtension = "json";
+            string folderName = "Tests";
+            //string inText = @"{""Name"":""John"",""Surname"":""Conor""}";
+            string inText = JsonConvert.SerializeObject(new BasicInfoEntity { Name = "John", Surname = "Connor" });
+            FileManager.WriteFile(fileName, fileExtension, inText, folderName);
+            string outText = FileManager.ReadFile(fileName, fileExtension, folderName);
+            Trace.WriteLine(string.Format("intText is: {0}", inText));
+            Trace.WriteLine(string.Format("outText is: {0}", outText));
+            Assert.AreEqual(inText,outText);
+        }
+    }
+}
---
+++ ThingsThatIForgetWhenISneeze.Application/FileManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThingsThatIForgetWhenISneeze.Application
+{
+    public static class FileManager
+    {
+        public static string GetPath(string fileName, string fileExtension,string folderPath="")
+        {
+            string path = null;
+            if(!string.IsNullOrWhiteSpace(fileName)&&!string.IsNullOrWhiteSpace(fileExtension))
+            {
+                string basePath = AppDomain.CurrentDomain.BaseDirectory;
+                
+                if (fileExtension.Contains("."))
+                {
+                    fileExtension=fileExtension.Replace(".", "");
+                }
+                if(string.IsNullOrWhiteSpace(folderPath))
+                {
+                    folderPath = "";
+                }
+                else if(folderPath[folderPath.Length-1]!='\\')
+                {
+                    folderPath += @"\";
+                    //this ensures directory creation
+                    if (!Directory.Exists(folderPath))
+                    {
+                        Directory.CreateDirectory(folderPath);
+                        
+                    }
+                }
+                path = string.Format(@"{0}\{1}{2}.{3}", basePath , folderPath,fileName,fileExtension);
+                /*
+                 * on asp.net it would be
+                 * path=HttpContext.Current.Server.MapPath("{0}{1}.{2}",folderPath,fileName,fileExtension);
+                 */
+
+                
+                //this ensures file creation
+                
+                var fs = File.Open(path, FileMode.OpenOrCreate);
+                fs.Close();
+                
+            }
+            return path;
+        }
+        public static void WriteFile(string fileName, string fileExtension,string fileContent, string folderPath = "")
+        {
+            string path = GetPath(fileName, fileExtension, folderPath);
+            if(path==null)
+            {
+                throw new InvalidOperationException("write file path was invalid");
+            }
+            
+            File.WriteAllText(path, fileContent);
+        }
+        public static string ReadFile(string fileName, string fileExtension, string folderPath = "")
+        {
+            return File.ReadAllText(GetPath(fileName, fileExtension, folderPath));
+        }
+    }
+}
